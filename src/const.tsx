6eef378f2618ebf4832bdca34548e210a2fcae04
Abishake01 <abishake381@gmail.com>
2025-09-28 04:50:56 +0530

    Register error

--- src/const.tsx
+++ src/const.tsx
@@ -1,2 +1,2 @@
-export const BD_PORT = 'https://backend-ombwttdrc-gokkull04s-projects.vercel.app';
+export const BD_PORT = 'https://localhost:5000';
 export const AI_URL_PORT = "https://ghost-test-qkc2.onrender.com";
